a43c5a7927303c2d005aabab907ac964ec75cd4b
Neha Sawant <bhaskar.csawant417@gmail.com>
2024-08-15 23:17:20 +0530

    js

---
+++ index.js
@@ -0,0 +1,54 @@
+/*              simple program to print
+let name = 5;
+console.log(name);
+
+*/
+
+
+
+/*              objects and its notation
+let person = {
+    name: 'neha',
+    age:20
+}
+//dot notation
+person.name = 'John';
+
+//bracket  Notation
+person['name'] = 'marry';
+
+console.log(person.name);
+*/
+
+
+
+ /*              Array
+let selectColors = ['red', 'green'];
+selectColors[3] = 'blue';
+
+console.log(selectColors);
+*/
+
+/*             Performing task to display smthing
+function greet(name, lname) {
+ 
+    console.log('Hello' + ' ' + name + ' ' +lname);
+}
+
+greet('Neha', 'Sawant');
+*/
+
+/*          Calculating a value
+function square(number) {
+    return number * number;
+}
+
+console.log(square(2));
+
+*/
+
+
+
+
+
+
